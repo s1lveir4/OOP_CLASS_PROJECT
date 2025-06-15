994ebaa80510f7531baceaa24063caa9933660d7
s1lveir4 <pedro.farias.silveira@gmail.com>
2025-06-14 23:04:35 -0300

    feat(shared): add Cnpj value class with format validation
    Create the Cnpj value class to encapsulate and validate CNPJ numbers.
    Includes format checking and ensures compliance with the official Brazilian structure.

---
+++ src/shared/Cnpj.java
@@ -0,0 +1,34 @@
+package shared;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import exceptions.CnpjException;
+
+public class Cnpj {
+	
+	private final String cnpj;
+	private final String validator = "^\\d{2}\\.\\d{3}\\.\\d{3}\\/\\d{4}\\-\\d{2}$";
+	
+	public Cnpj(String cnpj) throws CnpjException {
+		
+		if (validateCnpj(cnpj)) {
+			this.cnpj = cnpj;
+		}
+		else {
+			throw new CnpjException("Cnpj inválido");
+		}
+	}
+	
+	public boolean validateCnpj(String cnpj) {
+		
+		Pattern p = Pattern.compile(this.validator);
+		Matcher m = p.matcher(cnpj);
+		
+		if (m.matches()) {
+			return true;
+		}
+		
+		return false;
+	}
+}
