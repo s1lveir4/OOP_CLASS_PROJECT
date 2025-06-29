0450c6d145a936995203864e8f01dbd3b108d066
s1lveir4 <pedro.farias.silveira@gmail.com>
2025-06-29 10:39:09 -0300

    refactor(controllers.dtos): apply inheritance to pessoa-related DTOs
    Refactors PessoaFisicaDto and PessoaJuridicaDto to extend the new
    PessoaDto superclass.

--- src/controllers/dtos/PessoaFisicaDto.java
+++ src/controllers/dtos/PessoaFisicaDto.java
@@ -1,23 +1,16 @@
 package controllers.dtos;
 
-public class PessoaFisicaDto {
+public class PessoaFisicaDto extends PessoaDto {
 
 	private String cpf;
-	private String nome;
-	private String email;
-	private String telefone;
 
 	public PessoaFisicaDto() {}
-	
-	public PessoaFisicaDto(String cpf, String nome, String email, String telefone) {
-		
+
+	public PessoaFisicaDto(String nome, String email, String telefone, String cpf) {
+		super(nome, email, telefone);
 		this.cpf = cpf;
-		this.nome = nome;
-		this.email = email;
-		this.telefone = telefone;
-		
 	}
-	
+
 	public String getCpf() {
 		return cpf;
 	}
@@ -25,29 +18,4 @@ public class PessoaFisicaDto {
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getTelefone() {
-		return telefone;
-	}
-
-	public void setTelefone(String telefone) {
-		this.telefone = telefone;
-	}
-
 }
--- src/controllers/dtos/PessoaJuridicaDto.java
+++ src/controllers/dtos/PessoaJuridicaDto.java
@@ -1,32 +1,27 @@
 package controllers.dtos;
 
-public class PessoaJuridicaDto {
+public class PessoaJuridicaDto extends PessoaDto {
 
 	private String cnpj;
-	private String nome;
-	private String email;
-	private String telefone;
 
 	private String cpfPreposto;
 	private String nomePreposto;
 	private String emailPreposto;
 	private String telefonePreposto;
 
-	public PessoaJuridicaDto() {}
+	public PessoaJuridicaDto() {
+	}
 
-	public PessoaJuridicaDto(String cnpj, String nome, String email, String telefone, String cpfPreposto,
+	public PessoaJuridicaDto(String nome, String email, String telefone, String cnpj, String cpfPreposto,
 			String nomePreposto, String emailPreposto, String telefonePreposto) {
 		
+		super(nome, email, telefone);
 		this.cnpj = cnpj;
-		this.nome = nome;
-		this.email = email;
-		this.telefone = telefone;
-		
 		this.cpfPreposto = cpfPreposto;
 		this.nomePreposto = nomePreposto;
 		this.emailPreposto = emailPreposto;
 		this.telefonePreposto = telefonePreposto;
-		
+	
 	}
 
 	public String getCnpj() {
@@ -35,30 +30,6 @@ public class PessoaJuridicaDto {
 
 	public void setCnpj(String cnpj) {
 		this.cnpj = cnpj;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getTelefone() {
-		return telefone;
-	}
-
-	public void setTelefone(String telefone) {
-		this.telefone = telefone;
 	}
 
 	public String getCpfPreposto() {
@@ -92,6 +63,5 @@ public class PessoaJuridicaDto {
 	public void setTelefonePreposto(String telefonePreposto) {
 		this.telefonePreposto = telefonePreposto;
 	}
-	
-	
+
 }
